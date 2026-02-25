655aa0f5e9f51ccbabe0ce35be4946f2d7e7acf6
IshaPatel07 <Patelisha8067@gmail.com>
2026-02-25 15:22:26 +0530

    24/02/2026

---
+++ DAY-5/script.js
@@ -0,0 +1,45 @@
+//  wap to find minimum from 5 using nested if else
+
+const a=23, b=10, c=3, d=11, e=34;
+
+if(a<b){
+    if(a<c){
+        if(a<d){
+            if(a<e){
+                console.log("a is min")
+            }
+            else{
+                console.log("e is min")
+            }
+
+        }
+        else{
+            if(d<c){
+                if(d<b){
+                    if(d<e){
+                        console.log("d is min")
+                    }
+                    else{
+                        console.log("e is min")
+                    }
+
+                }
+                else{
+                    
+                }
+
+            }
+            else{
+
+            }
+        }
+
+    }
+    else{
+
+    }
+
+}
+else{
+
+}
