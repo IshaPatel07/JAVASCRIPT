b7d8b128ecf28663ad36de052d60ef404e6cefc2
IshaPatel07 <Patelisha8067@gmail.com>
2026-02-25 18:12:34 +0530

    25/02/2026

---
+++ DAY-7/script.js
@@ -0,0 +1,71 @@
+// wap toh 1 to n
+
+console.log("1.wap to print 1 to n");
+
+let i = 1; //  i hai vo starting ki value hai jaha se ye start hoga
+let n = 23; // n hai vo user input hai jisme user koee number dale or utni bar vo print ho sake
+
+while (i <= n) {
+  // i <= n ka mtln hai jab tak i ki value n se jyada nhi ho jati tab tak vo value return krta rhega
+  console.log(i);
+  // i ++ ka mtlb hai agr i ki value 1 hai toh vo n se choti hai toh usme 1 + hota rhega or jab tak vo n ki value tak na pohch jaye vo plus hoti rhegi
+  i++;
+}
+
+console.log(
+  "****************************************************************************************",
+);
+
+console.log("2.wap to print n to 1");
+// n se number ulte print hone chahiye like 10,9,8,7,6...
+
+let a = 1; // this value is ending value
+let b = 24; // and this value is starting value
+
+// is condition me hume piche se number lane hai jiske liye hume - karna hoga isliye hume jo minuss krna hai vo value ko hume pehle likhna hoga taki uss number mese kam hoke vo 1 tak ja sake
+
+while (b >= a) {
+  console.log(b);
+  b--;
+}
+
+console.log(
+  "****************************************************************************************",
+);
+
+console.log("3.wap to print only odd number");
+
+let c = 1;
+let d = 50;
+
+while (c <= d) {
+  console.log(c);
+  c += 2;
+}
+
+console.log(
+  "****************************************************************************************",
+);
+
+console.log("4.wap to print only even number");
+
+let e = 2;
+let f = 50;
+
+while (e <= f) {
+  console.log(e);
+  e += 2;
+}
+
+console.log(
+  "****************************************************************************************",
+);
+
+console.log("5.wap to print only leap year from 2000 to 3000.");
+
+let start = 2000;
+let end = 3000;
+
+while (start <= end) {
+  
+}
