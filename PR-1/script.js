11ea3a474c99ea5074a5773823840c65342e7363
IshaPatel07 <Patelisha8067@gmail.com>
2026-03-09 14:29:06 +0530

    09/03/2026

---
+++ PR-1/script.js
@@ -0,0 +1,68 @@
+console.log(
+  "1. write a javascript  program to swap two numbers using a third variable. ",
+);
+let a = 10;
+let b = 20;
+let u = a;
+a = b;
+b = u;
+console.log("a =", a);
+console.log("b =", b);
+
+console.log(
+  "2. write a javascript program to swap  two numbers without using third variable.",
+);
+
+let d = 23;
+let e = 11;
+
+d = d + e;
+e = d - e;
+d = d - e;
+
+console.log("d =", d);
+console.log("e =", e);
+
+// console.log(
+//   "3. write a javascript program to convert celsius  temprerture to fahrenheit.",
+// );
+
+// let c = 30;
+
+// let f = (c * 9 / 5) + 32;
+// console.log("fehrenheit is", f);
+
+// console.log(
+//   "4. write a javascript program to convert fehrenheit temprerture to celsius.",
+// );
+
+// let F = 60;
+
+// let C = ((c - 32) * 5) / 9;
+
+// console.log("celsius is", F);
+
+console.log(
+  "5. write a javascript program to calculate the area of rectengle using lenth and width.",
+);
+
+let lenth = 20;
+let width = 30;
+
+let area = lenth * width;
+
+console.log("rectangle area is =", area);
+
+console.log(
+  "7. write a javascript program to calculate the simple intrest using formula SI = (P*R*T)/100.",
+);
+
+let P = 20;
+let R = 10;
+let T = 30;
+
+let SI = (P * R * T) / 100;
+
+console.log("simple interest is =", SI);
+
+console.log("8. write a javascript program to calculate .");
