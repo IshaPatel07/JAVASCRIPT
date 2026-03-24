e27f8ae76e0a721986bd4efd568f37923614a1b6
IshaPatel07 <Patelisha8067@gmail.com>
2026-03-24 16:08:31 +0530

    24/03/2026

--- DAY-19/script.js
+++ DAY-19/script.js
@@ -1,4 +1,28 @@
-const buttons = document.getElementsByTagName("button");
+
+// DOM - document object model
+
+//html file -> object
+
+// nail cutter
+
+// we can control htnml/css using js
+
+//random bg colour generator
+//document.body.style.backgroundcolor ="black";
+
+
+// alert("Hello.....")
+
+const buttons= document.getElementsByTagName("button");//used to get element(tag)from document(inde3x.html) by tag name.
+
+// button = <button> Click </button>
+
+// buttons[0].textcontent ="name changed";//to change next content of any element.
+
+// bhutton[1].textContent ="button changed"
+
+// process = event
+// to add click on button,addEventlistener
 
 buttons[0].addEventListener("click", () => {
   const str = Math.floor(Math.random() * 16581375).toString(16);
