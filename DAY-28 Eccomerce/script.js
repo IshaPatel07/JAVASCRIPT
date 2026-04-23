4ff0e3ddcd8879cb2fef333e6ea2a09b85cf02f3
Isha patel <patelisha8067@gmail.com>
2026-04-23 17:27:34 +0530

    Update script.js

--- DAY-28 Eccomerce/script.js
+++ DAY-28 Eccomerce/script.js
@@ -1,14 +1,20 @@
-const productDivision = document.getElementById("product-div")
+const productDivision = document.getElementById("product-div");
+let allProduct = [];
 
 async function addToCard(i) {
-    const res = await fetch("https://dummyjson.com/products")
-    const data = await res.json();
-    // console.log(data.products[i]);
-    const arr = JSON.parse(localStorage.getItem("carts")) || [];
-    arr.push(data.products[i]);
-    localStorage.setItem("carts", JSON.stringify(arr));
-}
+  const arr = JSON.parse(localStorage.getItem("carts")) || [];
 
+  const res = arr.findIndex((e) => e.id == allProduct[i].id);
+
+  if (res == -1) {
+    allProduct[i].qty = 1;
+    arr.push(allProduct[i]);
+  } else {
+    arr[res].qty++;
+  }
+
+  localStorage.setItem("carts", JSON.stringify(arr));
+}
 
 function displayProducts(products) {
     products.map((product, i) => {
@@ -23,16 +29,16 @@ function displayProducts(products) {
                     <p class="card-text">${product.category}</p>
                     <p class="card-text">Price  ₹${(product.price * 86).toFixed(2)}</p>
                     <button onclick="addToCard(${i})" class="btn btn-primary">Add to card</button>
-                </div>`;
-        productDivision.appendChild(div);
-    })
+                </div>` ;
+    productDivision.appendChild(div);
+  });
 }
 
 async function fetchProduct() {
-    const res = await fetch("https://dummyjson.com/products");
-    const data = await res.json();
-    displayProducts(data.products)
-
+  const res = await fetch("https://dummyjson.com/products");
+  const data = await res.json();
+  allProduct = data.products;
+  displayProducts(data.products);
 }
 
 fetchProduct();
