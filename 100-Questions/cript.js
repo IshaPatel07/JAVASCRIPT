3e361589123d8c280d5f3b09bf58c9dbbf3834c7
dhruv-barapatre <dhruvbarapatre28012006@gmail.com>
2026-05-20 17:47:23 +0530

    20/05/2026

---
+++ 100-Questions/cript.js
@@ -0,0 +1,18 @@
+// num is positive negetive or zero
+
+// let n = 0;
+// if (n > 0) {
+//   console.log("number is positive!!");
+// } else if (n < 0) {
+//   console.log("number is negative");
+// } else {
+//   console.log("num is zero");
+// }
+
+// table of given number
+
+let num = 6;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(num * i);
+}
